a21f3fb3ce59ce9b9ef8b566e6186571761c9549
Alex Faissal <alefaissal@gmail.com>
2019-08-12 16:48:51 -0700

    add gitignore

--- themes/inhabitheme/inc/extras.php
+++ themes/inhabitheme/inc/extras.php
@@ -11,12 +11,58 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function red_starter_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+function red_starter_body_classes($classes)
+{
+    // Adds a class of group-blog to blogs with more than 1 published author.
+    if (is_multi_author()) {
+        $classes[] = 'group-blog';
+    }
 
-	return $classes;
+    //inhabitente_debug($classes);
+    if (is_singular('page')) {
+        global $post;
+        // inhabitente_debug($post);
+        $classes[] = 'page-' . $post->post_name;
+    }
+    // inhabitente_debug($classes);
+
+    return $classes;
 }
-add_filter( 'body_class', 'red_starter_body_classes' );
+add_filter('body_class', 'red_starter_body_classes');
+
+function inhabitent_remove_submenus()
+{
+    remove_submenu_page('themes.php', 'theme-editor.php');
+    remove_submenu_page('plugins.php', 'plugin-editor.php');
+}
+add_action('admin_menu', 'inhabitent_remove_submenus', 110);
+
+function inhabitente_debug($var)
+{
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+}
+
+// custom login for theme
+function custom_login()
+{
+    echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/customlogin.css" />';
+}
+
+add_action('login_head', 'custom_login');
+
+function my_custom_login_logo()
+{
+    echo '<style type="text/css">
+		#login h1 a { background-image:url(' . get_stylesheet_directory_uri() . '/images/logos/inhabitent-logo-text-dark.svg);
+		height: 53px; width: 300px; background-size: cover;}
+	</style>';
+}
+add_action('login_head', 'my_custom_login_logo');
+
+function my_custom_login_url($url)
+{
+    return get_bloginfo('url');
+}
+add_filter('login_headerurl', 'my_custom_login_url');
