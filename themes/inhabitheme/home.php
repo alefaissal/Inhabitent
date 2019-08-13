66e1cb806e0bb0e6ad3a8008ac353ccf6b0a8916
Alex Faissal <alefaissal@gmail.com>
2019-08-13 13:58:56 -0700

    add products taxonomy plugin

--- themes/inhabitheme/home.php
+++ themes/inhabitheme/home.php
@@ -21,23 +21,23 @@ get_header();?>
 			<?php /* Start the Loop */?>
 			<?php while (have_posts()): the_post();?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header journal-header">
-                        <?php if ( has_post_thumbnail() ) : ?>
-                            <?php the_post_thumbnail( 'large' ); ?>
-                        <?php endif; ?>
+		                <article class="article-journal" id="post-<?php the_ID();?>" <?php post_class();?>>
+		                    <header class="entry-header journal-header">
+		                        <?php if (has_post_thumbnail()): ?>
+		                            <?php the_post_thumbnail('large');?>
+		                        <?php endif;?>
 
-                        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                <?php the_title(sprintf('<h2 class="entry-title journal-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');?>
 
-                        <?php if ( 'post' === get_post_type() ) : ?>
-                        <div class="entry-meta">
-                            <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+                        <?php if ('post' === get_post_type()): ?>
+                        <div class="entry-meta journal-meta">
+                            <?php red_starter_posted_on();?> / <?php comments_number('0 Comments', '1 Comment', '% Comments');?> / <?php red_starter_posted_by();?>
                         </div><!-- .entry-meta -->
-                        <?php endif; ?>
+                        <?php endif;?>
                     </header><!-- .entry-header -->
 
-                    <div class="entry-content">
-                        <?php the_excerpt(); ?>
+                    <div class="entry-content journal-content">
+                        <?php the_excerpt();?>
                     </div><!-- .entry-content -->
                 </article><!-- #post-## -->
 
