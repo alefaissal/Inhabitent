f2ce35882a641abd5578e56528193ef5c6b997f8
Alex Faissal <alefaissal@gmail.com>
2019-08-13 17:02:23 -0700

    add css to footer, find us and about pages

--- themes/inhabitheme/footer.php
+++ themes/inhabitheme/footer.php
@@ -10,13 +10,34 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				<div class="site-info footer-box">
+					<div class="footer-container">
+						<div class="contact-box">
+							<p><span class="footer-title">Contact Info<span></p>
+							<p>info@inhabitent.com</p>
+							<p>778-456-7891</p>
+						</div>
+						<div class="hours-box">
+							<p><span class="footer-title">Business Hours<span></p>
+							<p>Monday-Friday: 9am to 5pm</p>
+							<p>Saturday: 10am to 2pm</p>
+							<p>Sunday: Closed</p>
+						</div>
+						<div class="logo-box">
+							<img src="http://localhost:8888/wordpress_projects/inhabitent//wp-content/themes/inhabitheme/images/logos/inhabitent-logo-text.svg" alt="inhabitent logo text">
+						</div>
+				</div>
+						<div class="copyright-box">
+							<p>Copyright <span>©2019</span> Inhabitent</p>
+						</div>
+
+
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+		<?php wp_footer();?>
 
 	</body>
 </html>
---
+++ themes/inhabitheme/page-about.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header();?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while (have_posts()): the_post();?>
+
+									<article id="post-<?php the_ID();?>" <?php post_class();?>>
+							<header class="entry-header">
+								<?php the_title('<h1 class="entry-title about-title">', '</h1>');?>
+							</header><!-- .entry-header -->
+
+							      <div class="entry-content about-content">
+							         	<?php the_content();?>
+							        	<?php
+    wp_link_pages(array(
+        'before' => '<div class="page-links">' . esc_html('Pages:'),
+        'after' => '</div>',
+    ));
+    ?>
+							     </div><!-- .entry-content -->
+						           </article><!-- #post-## -->
+
+
+									<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer();?>
