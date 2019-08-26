af338b434a528051c15b32cfae90e67e4699816b
Alex Faissal <alefaissal@gmail.com>
2019-08-26 15:46:19 -0700

    Add Css to search page

--- themes/inhabitheme/search.php
+++ themes/inhabitheme/search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages.
  *
@@ -7,32 +8,68 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+	<main id="main" class="site-main search-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+		<header class="page-header search-header">
+			<h1 class="page-title"><?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?></h1>
+		</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php /* Start the Loop */ ?>
+		<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+		<article class='search-article' id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
-			<?php endwhile; ?>
+				<?php if ('post' === get_post_type()) : ?>
+				<div class="entry-meta">
+					<?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
 
-			<?php red_starter_numbered_pagination(); ?>
+			<div class="entry-summary search-text">
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-summary -->
+		</article><!-- #post-## -->
+
+		<?php endwhile; ?>
+
+		<?php red_starter_numbered_pagination(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+		<section class="no-results not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html('Nothing Found'); ?></h1>
+			</header><!-- .page-header -->
+
+			<div class="page-content search-not-fund-text">
+				<?php if (is_home() && current_user_can('publish_posts')) : ?>
+
+				<p><?php printf(wp_kses('Ready to publish your first post? <a href="%1$s">Get started here</a>.', array('a' => array('href' => array()))), esc_url(admin_url('post-new.php'))); ?></p>
+
+				<?php elseif (is_search()) : ?>
+
+				<p><?php echo esc_html('Sorry, but nothing matched your search terms. Please try again with some different keywords.'); ?></p>
+				<?php get_search_form(); ?>
+
+				<?php else : ?>
+
+				<p><?php echo esc_html('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.'); ?></p>
+				<?php get_search_form(); ?>
+
+				<?php endif; ?>
+			</div><!-- .page-content -->
+		</section><!-- .no-results -->
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main><!-- #main -->
+</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
