5863715d5e8adc95cb44283a113a27114189fb95
Alex Faissal <alefaissal@gmail.com>
2019-08-27 15:57:03 -0700

    Add more CSS to single product page to align title and img on top

--- themes/inhabitheme/js/script.js
+++ themes/inhabitheme/js/script.js
@@ -5,21 +5,33 @@
 
 
     $(function() {
+      let isSearchOpen = false;
+
       const $input = $('.search-field');
       const $searchBar = $('.header-search-bar');
+
       $('.site-header .icon-search').on('click', function() {
+        isSearchOpen = true;
         if ($input.val().length === 0) {
             event.preventDefault();
           }
         $searchBar.toggleClass('search-on');
         $input.focus();
       });
+
+      // setTimeout(function(){
+
+      // }, 500);
   
       $input.on('blur', function () { 
-        if ($input.val().length > 0) {
-          return false;
+        if (!$input.val().length > 0 && isSearchOpen) {
+          console.log('removed class');
+          $searchBar.removeClass('search-on');
+          isSearchOpen = false;
+        } 
+        else {
+          isSearchOpen = true;
         }
-        $searchBar.removeClass('search-on');
       });
     });
 
